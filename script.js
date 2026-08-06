86a4fd71d3cc04e5081eeba6b6084a7736b35f37
jamesdmitchell-source <jamesdmitchell@btinternet.com>
2026-08-06 01:19:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    const  startButton = document.getElementById("startButton");
+    
+    startButton.addEventListener("click", function  (). {
+    
+         alert ("Welcome to THE RIGHT PATH.\n\nThe trial begins.....");
+         
+     });
+     
+ });
