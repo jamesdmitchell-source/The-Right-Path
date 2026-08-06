dc7aed86cfadf9620b9eabe02af3067184e093f2
jamesdmitchell-source <jamesdmitchell@btinternet.com>
2026-08-06 02:28:11 +0100

    Fix syntax error in click event listener

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
  
     const  startButton = document.getElementById("startButton");
     
-    startButton.addEventListener("click", function  (). {
+    startButton.addEventListener("click", function  () {
     
          alert ("Welcome to THE RIGHT PATH.\n\nThe trial begins.....");
          
